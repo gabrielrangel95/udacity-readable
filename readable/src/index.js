0a949d6f91b7f4f04193d99270395728d6022ee8
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-05-07 03:25:44 -0300

    feat: router implemented on root  index.js

--- readable/src/index.js
+++ readable/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './redux/store';
@@ -8,7 +9,9 @@ import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root'),
 );
