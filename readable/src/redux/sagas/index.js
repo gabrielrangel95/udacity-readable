b86e8650657f3465b2abdd04e12a46d6f5683749
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-05-19 20:25:39 -0300

    ref: Sagas with posts

--- readable/src/redux/sagas/index.js
+++ readable/src/redux/sagas/index.js
@@ -1,9 +1,14 @@
 import { all, takeLatest } from 'redux-saga/effects';
+
 import { Types as CategoriesTypes } from '../ducks/Categories';
+import { Types as PostTypes } from '../ducks/Posts';
+
 import { getCategories } from './Categories';
+import { getPosts } from './Posts';
 
 export default function* rootSaga() {
   return yield all([
     takeLatest(CategoriesTypes.GET_REQUEST, getCategories),
+    takeLatest(PostTypes.GET_REQUEST, getPosts),
   ]);
 }
