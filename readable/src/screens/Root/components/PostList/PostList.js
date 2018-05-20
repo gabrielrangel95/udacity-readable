d87c0116acc12faca6abecdfedbd8cb54f900c45
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-05-20 13:07:25 -0300

    ref: PostList use PostItem

--- readable/src/screens/Root/components/PostList/PostList.js
+++ readable/src/screens/Root/components/PostList/PostList.js
@@ -2,16 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
-import { List, Icon } from 'antd';
+import { List } from 'antd';
 import PropTypes from 'prop-types';
+import { PostItem } from '../../../../components';
 
 import { Creators as postsCreators } from '../../../../redux/ducks/Posts';
 import './PostList.css';
-
-const marvelLogo = require('../../../../resources/img/marvel.svg');
-const dcLogo = require('../../../../resources/img/dc.svg');
-const discussLogo = require('../../../../resources/img/di_logo.svg');
-
 
 class PostList extends Component {
   static propTypes = {
@@ -35,76 +31,6 @@ class PostList extends Component {
   componentDidMount() {
     this.props.getPostsRequest();
   }
-
-
-  categorieItemImage = (item) => {
-    if (item.category === 'dc') {
-      return (
-        <div className="div-categorie">
-          <img src={dcLogo} className="image-categorie" alt="dc" />
-          <span className="text-categorie">{item.category}</span>
-        </div>
-      );
-    } else if (item.category === 'marvel') {
-      return (
-        <div className="div-categorie">
-          <img src={marvelLogo} className="image-categorie" alt="marvelLogo" />
-          <span className="text-categorie">{item.category}</span>
-        </div>
-      );
-    }
-    return (
-      <div className="div-categorie">
-        <img src={discussLogo} className="image-categorie" alt="discussLogo" />
-        <span className="text-categorie">{item.category}</span>
-      </div>
-    );
-  }
-
-  categorieRenderText = item => (
-    <div className="div-post-texts">
-      <span className="text-item-title">{item.title}</span>
-      <span>{item.body}</span>
-    </div>
-  )
-
-  categorieDetails = item => (
-    <div className="div-post-details">
-      <span>
-        <Icon type="user" className="icon-details" />
-        {item.author}
-      </span>
-      <span>
-        <Icon type="message" className="icon-details" />
-        {item.commentCount}
-      </span>
-      <span>
-        <Icon type="clock-circle-o" className="icon-details" />
-        {new Date(item.timestamp).toDateString()}
-      </span>
-    </div>
-  )
-
-  categorieActions = () => (
-    <div className="div-post-actions">
-      <span>
-        <Icon type="edit" className="icon-details" />
-        Edit
-      </span>
-      <span>
-        <Icon type="delete" className="icon-details" />
-        Delete
-      </span>
-    </div>
-  )
-
-  categorieVoteScore = item => (
-    <div className="div-score">
-      <Icon type="up-square-o" className="icon-score" />
-      <span className="text-score">{item.voteScore}</span>
-      <Icon type="down-square-o" className="icon-score" />
-    </div>
-  )
 
   render() {
     const { data } = this.props.posts;
@@ -139,23 +65,9 @@ class PostList extends Component {
             pageSize: 10,
           }}
           renderItem={item => (
-            <List.Item key={item.title} >
-              <div>
-                {this.categorieItemImage(item)}
-              </div>
-              <div>
-                {this.categorieRenderText(item)}
-              </div>
-              <div>
-                {this.categorieDetails(item)}
-              </div>
-              <div>
-                {this.categorieVoteScore(item)}
-              </div>
-              <div>
-                {this.categorieActions(item)}
-              </div>
-            </List.Item>
+            <PostItem
+              item={item}
+            />
           )}
         />
       </div>
