42ab39714515e2c9234dc7265f61c3db9f6727fa
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-05-20 00:19:53 -0300

    ref: defaulData change

--- api-server/posts.js
+++ api-server/posts.js
@@ -6,10 +6,10 @@ const defaultData = {
   "8xf0y6ziyjabvozdd253nd": {
     id: '8xf0y6ziyjabvozdd253nd',
     timestamp: 1467166872634,
-    title: 'Udacity is the best place to learn React',
-    body: 'Everyone says so after all.',
-    author: 'thingtwo',
-    category: 'react',
+    title: 'Is Bruce Wayne Batman ? ',
+    body: 'Some little bird told me that',
+    author: 'deadpool',
+    category: 'dc',
     voteScore: 6,
     deleted: false,
     commentCount: 2
@@ -17,10 +17,10 @@ const defaultData = {
   "6ni6ok3ym7mf1p33lnez": {
     id: '6ni6ok3ym7mf1p33lnez',
     timestamp: 1468479767190,
-    title: 'Learn Redux in 10 minutes!',
-    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
-    author: 'thingone',
-    category: 'redux',
+    title: 'Who is faster: Mercurio or Flash' ,
+    body: 'Maybe Kid Flash could join on that question',
+    author: 'Cisco',
+    category: 'discussion',
     voteScore: -5,
     deleted: false,
     commentCount: 0
