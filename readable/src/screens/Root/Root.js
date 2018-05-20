bce8217763c4caa44cbad3881f95be3df33fb528
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-05-20 00:23:22 -0300

    ref: Root use new components

--- readable/src/screens/Root/Root.js
+++ readable/src/screens/Root/Root.js
@@ -1,154 +1,39 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { bindActionCreators } from 'redux';
-import { List, Avatar, Icon, Row, Col } from 'antd';
-
-import { Creators as postsCreators } from '../../redux/ducks/Posts';
+import { Button } from 'antd';
 import './Root.css';
-
-const marvelLogo = require('../../resources/img/marvel.svg');
-const dcLogo = require('../../resources/img/dc.svg');
-const discussLogo = require('../../resources/img/di_logo.svg');
-
+import { PostList, NewPostModal } from './components';
 
 class Root extends Component {
-  componentDidMount() {
-    this.props.getPostsRequest();
+  state = {
+    createModalVisible: false,
   }
 
-  componentWillReceiveProps(nextProps) {
-    console.log(nextProps);
+  onModalCancel = () => {
+    this.setState({ createModalVisible: false });
   }
 
-  categorieItemImage = (item) => {
-    if (item.category === 'dc') {
-      return (
-        <div className="div-categorie">
-          <img src={dcLogo} className="image-categorie" alt="dc" />
-          <span className="text-categorie">{item.category}</span>
-        </div>
-      );
-    } else if (item.category === 'marvel') {
-      return (
-        <div className="div-categorie">
-          <img src={marvelLogo} className="image-categorie" alt="marvelLogo" />
-          <span className="text-categorie">{item.category}</span>
-        </div>
-      );
-    }
-    return (
-      <div className="div-categorie">
-        <img src={discussLogo} className="image-categorie" alt="discussLogo" />
-        <span className="text-categorie">{item.category}</span>
-      </div>
-    );
+  openCreateModal = () => {
+    this.setState({ createModalVisible: true });
   }
-
-  categorieRenderText = item => (
-    <div className="div-post-texts">
-      <span className="text-item-title">{item.title}</span>
-      <span>{item.body}</span>
-    </div>
-  )
-
-  categorieDetails = item => (
-    <div className="div-post-details">
-      <span>
-        <Icon type="user" className="icon-details" />
-        {item.author}
-      </span>
-      <span>
-        <Icon type="message" className="icon-details" />
-        {item.commentCount}
-      </span>
-      <span>
-        <Icon type="clock-circle-o" className="icon-details" />
-        {new Date(item.timestamp).toDateString()}
-      </span>
-    </div>
-  )
-
-  categorieActions = item => (
-    <div className="div-post-actions">
-      <span>
-        <Icon type="edit" className="icon-details" />
-        Edit
-      </span>
-      <span>
-        <Icon type="delete" className="icon-details" />
-        Delete
-      </span>
-    </div>
-  )
-
-  categorieVoteScore = item => (
-    <div className="div-score">
-      <Icon type="up-square-o" className="icon-score" />
-      <span className="text-score">{item.voteScore}</span>
-      <Icon type="down-square-o" className="icon-score" />
-    </div>
-  )
 
   render() {
-    const { data } = this.props.posts;
     return (
       <div>
-        <h2>All Posts</h2>
-        <div>
-          <List.Item>
-            <div className="title-categorie">
-              <span>Category</span>
-            </div>
-            <div className="title-description">
-              <span>Description</span>
-            </div>
-            <div className="title-details">
-              <span>Details</span>
-            </div>
-            <div className="title-score">
-              <span>Score</span>
-            </div>
-            <div className="title-actions">
-              <span>Actions</span>
-            </div>
-          </List.Item>
+        <h1>All Posts</h1>
+        <div className="div-button-add-post">
+          <Button onClick={() => this.openCreateModal()}>New Post</Button>
         </div>
-        <List
-          itemLayout="horizontal"
-          dataSource={data}
-          renderItem={item => (
-            <List.Item key={item.title} >
-              <div>
-                {this.categorieItemImage(item)}
-              </div>
-              <div>
-                {this.categorieRenderText(item)}
-              </div>
-              <div>
-                {this.categorieDetails(item)}
-              </div>
-              <div>
-                {this.categorieVoteScore(item)}
-              </div>
-              <div>
-                {this.categorieActions(item)}
-              </div>
-            </List.Item>
-          )}
+        <PostList />
+        <NewPostModal
+          visible={this.state.createModalVisible}
+          onCancel={this.onModalCancel}
         />
       </div>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  posts: state.posts,
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(postsCreators, dispatch);
-
-const RootWithRouter = withRouter(connect(mapStateToProps, mapDispatchToProps)(Root));
+const RootWithRouter = withRouter(Root);
 export { RootWithRouter as Root };
 
