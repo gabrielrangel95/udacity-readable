8cf3c68272f87680e76324cb4c5a023b96037df0
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-05-20 01:12:31 -0300

    feat: implement filters

--- readable/src/screens/Root/Root.js
+++ readable/src/screens/Root/Root.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Button } from 'antd';
+import { bindActionCreators } from 'redux';
+import { Button, Select } from 'antd';
 import './Root.css';
 import { PostList, NewPostModal } from './components';
+import { Creators as postsCreators } from '../../redux/ducks/Posts';
+
+const { Option } = Select;
 
 class Root extends Component {
   state = {
@@ -17,12 +22,21 @@ class Root extends Component {
     this.setState({ createModalVisible: true });
   }
 
+  handleSelectChange = (value) => {
+    console.log(`selected ${value}`);
+    this.props.sortPostRequest(value);
+  }
+
   render() {
     return (
       <div>
         <h1>All Posts</h1>
-        <div className="div-button-add-post">
-          <Button onClick={() => this.openCreateModal()}>New Post</Button>
+        <div className="div-buttons">
+          <Select className="div-select-filters" onChange={this.handleSelectChange} placeholder="Filter">
+            <Option value="voteScore">voteScore</Option>
+            <Option value="timestamp">Date</Option>
+          </Select>
+          <Button onClick={() => this.openCreateModal()} className="div-button-new-post">New Post</Button>
         </div>
         <PostList />
         <NewPostModal
@@ -34,6 +48,14 @@ class Root extends Component {
   }
 }
 
-const RootWithRouter = withRouter(Root);
+const mapStateToProps = state => ({
+  posts: state.posts,
+});
+
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(postsCreators, dispatch);
+
+const RootWithRouter = withRouter(connect(mapStateToProps, mapDispatchToProps)(Root));
 export { RootWithRouter as Root };
 
