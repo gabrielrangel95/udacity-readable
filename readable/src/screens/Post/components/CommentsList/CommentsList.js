f841af5a391d8a35d4256b9c213ef0c95625d489
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-05-20 18:10:14 -0300

    feat: CommnetList create

---
+++ readable/src/screens/Post/components/CommentsList/CommentsList.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { List } from 'antd';
+import PropTypes from 'prop-types';
+import { Creators as commentsCreators } from '../../../../redux/ducks/Comments';
+import './CommentsList.css';
+import { CommentItem } from '../';
+
+class CommentsList extends Component {
+  static propTypes = {
+    getCommentsRequest: PropTypes.func.isRequired,
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        id: PropTypes.string,
+      }),
+    }).isRequired,
+    comments: PropTypes.shape({
+      data: PropTypes.arrayOf(PropTypes.shape({
+        author: PropTypes.string,
+        body: PropTypes.string,
+        deleted: PropTypes.bool,
+        id: PropTypes.string,
+        parentDeleted: PropTypes.bool,
+        parentId: PropTypes.string,
+        timestamp: PropTypes.number,
+        voteScore: PropTypes.number,
+      })),
+      loading: PropTypes.bool,
+    }).isRequired,
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getCommentsRequest(id);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps);
+  }
+
+
+  render() {
+    const { data } = this.props.comments;
+    return (
+      <div>
+        <h1>Comments: </h1>
+        <List
+          itemLayout="horizontal"
+          dataSource={data}
+          pagination={{
+            onChange: (page) => {
+              console.log(page);
+            },
+            pageSize: 10,
+          }}
+          renderItem={item => (
+            <CommentItem
+              item={item}
+            />
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  comments: state.comments,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(commentsCreators, dispatch);
+
+const CommentsListConnect = withRouter(connect(mapStateToProps, mapDispatchToProps)(CommentsList));
+export { CommentsListConnect as CommentsList };
+
