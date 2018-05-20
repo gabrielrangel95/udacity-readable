28b2b05944bfecb09ee1119e7771870fbc860cf4
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-05-20 13:06:48 -0300

    feat: Single post page

---
+++ readable/src/screens/Post/Post.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import './Post.css';
+import { Creators as postsCreators } from '../../redux/ducks/Posts';
+import { PostItem } from '../../components';
+
+class Post extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        id: PropTypes.string,
+      }),
+    }).isRequired,
+    selected: PropTypes.shape(),
+    getSinglePostRequest: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    selected: null,
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getSinglePostRequest(id);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps);
+  }
+
+  render() {
+    const { selected } = this.props;
+    return (
+      <div>
+        <h1>Post</h1>
+        {
+          selected && (
+            <div className="div-post-selected ">
+              <PostItem
+                item={selected}
+              />
+            </div>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  selected: state.posts.selected,
+});
+
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(postsCreators, dispatch);
+
+const PostWithRouter = withRouter(connect(mapStateToProps, mapDispatchToProps)(Post));
+export { PostWithRouter as Post };
+
