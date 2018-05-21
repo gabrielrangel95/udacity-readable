d4f28509f41267ebd61a91eb434770b51f5e1906
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-05-21 16:36:15 -0300

    feat: CommentModal implement

--- readable/src/screens/Post/Post.js
+++ readable/src/screens/Post/Post.js
@@ -3,10 +3,11 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
+import { Button } from 'antd';
 import './Post.css';
 import { Creators as postsCreators } from '../../redux/ducks/Posts';
 import { PostItem, PostHeader } from '../../components';
-import { CommentsList } from './components';
+import { CommentsList, CommentModal } from './components';
 
 class Post extends Component {
   static propTypes = {
@@ -23,20 +24,43 @@ class Post extends Component {
     selected: null,
   }
 
+  state = {
+    modalVisible: false,
+    madalTitle: 'New Comment',
+  }
+
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.getSinglePostRequest(id);
   }
 
+  openCreateModal = () => {
+    this.setState({ modalVisible: true, madalTitle: 'New Comment' });
+  }
+
+  closeModal = () => {
+    this.setState({ modalVisible: false });
+  }
+
 
   render() {
     const { selected } = this.props;
+    const { id } = this.props.match.params;
     return (
       <div>
         <h1>Post</h1>
+        <div className="div-button-add-comment">
+          <Button type="danger" onClick={() => this.openCreateModal()}>New Comment</Button>
+        </div>
         {
           selected && (
             <div>
+              <CommentModal
+                visible={this.state.modalVisible}
+                modalTitle={this.state.madalTitle}
+                onCancel={this.closeModal}
+                parentId={id}
+              />
               <div className="div-post-selected ">
                 <PostHeader />
                 <PostItem
