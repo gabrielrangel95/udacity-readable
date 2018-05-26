cdb0c36272c49c5c4ccf43021e65328ad4740cba
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-05-26 11:24:19 -0300

    feat: 404 route implement

--- readable/src/App.js
+++ readable/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import { MainContainer } from './components';
-import { Root, Post } from './screens';
+import { Root, Post, NotFound } from './screens';
 
 const App = () => (
   <MainContainer>
     <Switch>
       <Route exact path="/" component={Root} />
+      <Route exact path="/404" component={NotFound} />
       <Route exact path="/:category" component={Root} />
       <Route exact path="/:category/:id" component={Post} />
     </Switch>
