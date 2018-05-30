b365106ee69cba2f673d347b3e4fac8fb9fd5dc1
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-05-30 20:02:31 -0300

    feat: generate token for each user

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
-
 const url = 'https://powerful-depths-95152.herokuapp.com/';
+
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
 
 const api = axios.create({
   baseURL: `${url}`,
   headers: {
-    Authorization: '-93824u2bnf349uh34-b34ubn',
+    Authorization: token,
   },
 });
 
